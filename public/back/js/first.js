ab4e8167cb1453e747d9aca4c119204718ac5960
fwd001 <591738494@qq.com>
2018-06-19 19:45:15 +0800

    一级分类完成

---
+++ public/back/js/first.js
@@ -0,0 +1,70 @@
+$(function () {
+  var page = 1;
+  var pageSize = 8;
+  render();
+
+  function render() {
+    $.ajax({
+      type: 'get',
+      url: '/category/queryTopCategoryPaging',
+      data: {
+        page: page,
+        pageSize: pageSize
+      },
+      success: function (info) {
+
+        $('tbody').html(template('tpl', info));
+        $('#paginator').bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          currentPage: page,
+          totalPages: Math.ceil(info.total / info.size),
+          size: 'small',
+          onPageClicked: function (a, b, c, p) {
+            page = p,
+              render();
+          }
+        })
+      }
+    })
+  }
+
+  // 添加功能
+  $('form').bootstrapValidator({
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      categoryName: {
+        validators: {
+          notEmpty: {
+            message: '分类名不能为空'
+          }
+        }
+      }
+    }
+  })
+  var validator = $("#form").data('bootstrapValidator'); 
+  $('.btn_addCategroy').on('click', function () {
+    $('#addModal').modal('show');
+  })
+
+  
+  $('form').on('success.form.bv', function (e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: 'post',
+      url: '/category/addTopCategory',
+      data: $('form').serialize(),
+      success: function (info) {
+        if (info.success) {
+          $('#addModal').modal('hide');
+          $('form')[0].reset();
+          render();
+        }
+      }
+    })
+  })
+})
